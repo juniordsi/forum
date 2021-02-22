c9c15d787162de04e63cc8e31ae7a1b5248ca034
Junior Santos <junior.santos@dcomp.ufs.br>
2021-02-22 08:11:19 -0300

    Autenticação de usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$27p0NdhgFo9qr0nQ/7Ynkemr4TuYpnY4cmJXH06CJmXimIdwn1swm');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
